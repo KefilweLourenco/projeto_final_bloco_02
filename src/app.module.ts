dc4855f7528977c3f596fbb5988fad3f37019fcb
Kefilwe Lourenco <kefilwebr@hotmail.com>
2026-04-16 14:09:22 -0300

    feat: inicia relacionamento entre produto e categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriaModule } from './categoria/categoria.module';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { CategoriaModule } from './categoria/categoria.module';
       synchronize: true,
     }),
     CategoriaModule,
+    ProdutoModule
   ],
   controllers: [],
   providers: [],
